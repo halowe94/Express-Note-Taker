652c6484d3cde4762d4c0e67abbe7862c1f5d00e
H Lowe <haleylowe0@gmail.com>
2020-09-25 20:51:55 -0700

    added to get and post requests

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,18 +3,28 @@ const fs = require('fs');
 
 const dbVar = path.join(__dirname, '../', 'db');
 
+//Module.export api.routes
 module.exports = function(app) {
+    //GET request reads db.json
     app.get('/api/notes', (req, res) => {
+        let json = json;
+        let data = fs.readFileSync(path.join(databaseDir, "db.json"), "utf8");
+        json = JSON.parse(data);
+    });
+
+    //POST request recieves new note to save on the request body
+    //add post to db
+    app.post('/api/notes', (req, res) => {
+        let data = req.body;
+        let noteData = fs.readFilesync(path.join(databaseDir, "dbjson"), "utf8");
+        let dbData = JSON.parse(noteData);
+    
+    });
+
+    //read all notes and delete via id
+    app.delete('api/notes', (req, res) => {
         
     });
+
 };
-
-app.post('/api/notes', (req, res) => {
-    
-});
-
-app.delete('api/notes', (req, res) => {
-
-});
-
 
