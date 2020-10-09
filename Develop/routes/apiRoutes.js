b3e8cd11074cc391f3e964dd503dd61f350ded6a
H Lowe <haleylowe0@gmail.com>
2020-10-09 13:48:25 -0700

    html routes for index and notes htmls

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,24 +1,28 @@
 const path = require('path');
 const fs = require('fs');
+const router = require('express').Router();
+
 
 //checkObject = dbVar
 const dbVar = path.join(__dirname, '../', 'db');
 
 //Module.export api.routes
-module.exports = function(app) {
+
     //GET request reads db.json
-    app.get('/api/notes', (req, res) => {
+    router.get('/api/notes', (req, res) => {
         let json;
-        let data = fs.readFileSync(path.join(databaseDir, "db.json"), "utf8");
+        let data = fs.readFileSync(path.join(dbVar, "db.json"), "utf8");
         json = JSON.parse(data);
+        res.json(json);
     });
 
     //POST request recieves new note to save on the request body
     //add post to db
-    app.post('/api/notes', (req, res) => {
+    router.post('/api/notes', (req, res) => {
         let data = req.body;
-        let noteData = fs.readFilesync(path.join(databaseDir, "dbjson"), "utf8");
+        let noteData = fs.readFileSync(path.join(dbVar, "db.json"), "utf8");
         let dbData = JSON.parse(noteData);
+        console.log(data);
 
         //for loop to increment id
         for (i = 0; i < dbData.length, i++;) {
@@ -31,10 +35,36 @@ module.exports = function(app) {
     });
 
     //read all notes and delete via id
-    app.delete('api/notes', (req, res) => {
+    router.delete('api/notes', (req, res) => {
         
     });
 
-};
+    module.exports = router;
 
 
+
+    // app.get('/notes', (req, res) => {
+    //     res.json()
+    // });
+    
+    // //Get SINGLE member
+    // app.get("/api/notes/:notes", (req, res) => {
+    //     res.json()
+    // });
+    
+    // app.post("/api/notes", (req, res) => {
+    //     let newNote = req.body;
+    //     notes.push(newNote);
+    //     res.json(newNote);
+    // });
+    
+    // app.delete("/api/notes/:notes", (req, res) => {
+    //     res.json()
+    // });
+    
+
+    // router.get('/user/:id', function (req, res) {
+    //     res.send('OK')
+    //   })
+      
+    //   app.use(router)
--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const path = require('path');
 const app = express();
 const fs = require('fs');
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 //we want to look for the environment variables and we want to look for one called port
 let PORT = process.env.PORT || 8080
@@ -11,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //set static folder
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", htmlRoutes);
+app.use("/", apiRoutes);
 
 
 // just going to a webpage is a get request
@@ -22,23 +26,23 @@ app.use(express.static(path.join(__dirname, "public")))
 //api route
 //only one expression means no curly braces
 //Get ALL members
-app.get('/notes', (req, res) => {
-    res.json()
-});
+// app.get('/notes', (req, res) => {
+//     res.json()
+// });
 
-//Get SINGLE member
-app.get("/api/notes/:notes", (req, res) => {
-    res.json()
-});
+// //Get SINGLE member
+// app.get("/api/notes/:notes", (req, res) => {
+//     res.json()
+// });
 
-app.post("/api/notes", (req, res) => {
-    let newNote = req.body;
-    notes.push(newNote);
-    res.json(newNote);
-});
+// app.post("/api/notes", (req, res) => {
+//     let newNote = req.body;
+//     notes.push(newNote);
+//     res.json(newNote);
+// });
 
-app.delete("/api/notes/:notes", (req, res) => {
-    res.json()
-});
+// app.delete("/api/notes/:notes", (req, res) => {
+//     res.json()
+// });
 
 app.listen(PORT, () => {console.log("server started on PORT " + PORT)});
