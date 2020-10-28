d9297d449e2271af3d1a248b7efee85200b1018d
H Lowe <haleylowe0@gmail.com>
2020-10-27 23:11:03 -0700

    deleted comments from server

--- Develop/server.js
+++ Develop/server.js
@@ -17,32 +17,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", htmlRoutes);
 app.use("/", apiRoutes);
 
-
-// just going to a webpage is a get request
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-//api route
-//only one expression means no curly braces
-//Get ALL members
-// app.get('/notes', (req, res) => {
-//     res.json()
-// });
-
-// //Get SINGLE member
-// app.get("/api/notes/:notes", (req, res) => {
-//     res.json()
-// });
-
-// app.post("/api/notes", (req, res) => {
-//     let newNote = req.body;
-//     notes.push(newNote);
-//     res.json(newNote);
-// });
-
-// app.delete("/api/notes/:notes", (req, res) => {
-//     res.json()
-// });
-
 app.listen(PORT, () => {console.log("server started on PORT " + PORT)});
